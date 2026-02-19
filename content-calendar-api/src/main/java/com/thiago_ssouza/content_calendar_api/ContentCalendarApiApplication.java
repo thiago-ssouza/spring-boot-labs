0cd2a24a7a26ae7023083050a158c6eb4051ba9e
Thiago Soares de Souza <thiago.souza.jobs@gmail.com>
2026-02-19 13:43:04 -0500

    Initial commit created project content-calendar-api

---
+++ content-calendar-api/src/main/java/com/thiago_ssouza/content_calendar_api/ContentCalendarApiApplication.java
@@ -0,0 +1,13 @@
+package com.thiago_ssouza.content_calendar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalendarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCalendarApiApplication.class, args);
+	}
+
+}
---
+++ content-calendar-api/src/test/java/com/thiago_ssouza/content_calendar_api/ContentCalendarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago_ssouza.content_calendar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalendarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
